83413bcde4d46d3be0805ff956aea440e32b613f
Hemaka <hemakaseneviratne@gmail.com>
2022-09-23 17:37:30 +0530

    initial bundled commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x704e7DdF05308dA9a0c6C73d756BDF0f7da9be8f";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
